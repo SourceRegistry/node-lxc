3305b6ff12b12766f5cc0663d8f17116560eab31
A.P.A. Slaa <a.p.a.slaa@projectsource.nl>
2026-05-26 11:07:24 +0000

    chore(example): testing attach example

--- examples/attach/index.ts
+++ examples/attach/index.ts
@@ -1,3 +1,4 @@
+import {execSync} from "child_process";
 import {Container} from "../../lib";
 
 const name = "test-ct"
@@ -6,6 +7,9 @@ const name = "test-ct"
  * In this example, a container is created and the calling process stdio is attached.
  */
 async function main() {
+    // Start lxc-net so dnsmasq provides DHCP on lxcbr0 (10.0.3.0/24)
+    // execSync("service lxc-net start", {stdio: "ignore"});
+
     const c = new Container(name);
     if (!c.defined) {
         console.log("Container creating...");
@@ -21,6 +25,7 @@ async function main() {
     console.log(`Attaching to container '${name}'`)
     await c.attach({
         stdio: [process.stdin.fd, process.stdout.fd, process.stderr.fd],
+        extra_env_vars: [`TERM=${process.env.TERM ?? "xterm"}`],
     });
     console.warn(`Destroying '${name}'`)
     await c.destroy({force: true});
